1eeb908d2ecc20daa77392c4c086ef5d4f1b5497
Robert <s3901069@student.rmit.edu.au>
2023-08-09 14:53:51 +1000

    Registration database entry complete

--- noaction.php
+++ noaction.php
@@ -1,17 +1,26 @@
 <?php
+
 	$fname = $_POST['fname'];
 	$email = $_POST['email'];
 	$phone = $_POST['phone'];
+	$age = $_POST['age'];
+	$gender = $_POST['gender'];
 	$password = $_POST['password'];
+	$date = $_POST['date'];
+	$heard_about_us = $_POST['heard_about_us'];
+	$bio = $_POST['bio'];
+
+	// Hash the password
+	$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
 	// Database connection
-	$conn = new mysqli('localhost', 'root', 'enter the password here', 'test');
+	$conn = new mysqli('localhost', 'root', 'Secret123!', 'test');
 	if ($conn->connect_error) {
 		echo "$conn->connect_error";
 		die("Connection Failed: " . $conn->connect_error);
 	} else {
-		$stmt = $conn->prepare("INSERT INTO persons(fname, email, phone, password) VALUES(?, ?, ?, ?)");
-		$stmt->bind_param("ssss", $fname, $email, $phone, $password);  // Removed the extra 'i'
+		$stmt = $conn->prepare("INSERT INTO persons(fname, email, phone, age, gender, password, date, heard_about_us, bio) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("sssssssss", $fname, $email, $phone, $age, $gender, $hashedPassword, $date, $heard_about_us, $bio);
 		$execval = $stmt->execute();
 		if ($execval) {
 			echo "Registration successful...";
