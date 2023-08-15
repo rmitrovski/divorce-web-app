147fa8e51da9a9c4a16569950248be94c567ec67
Mariam Hanna <115123087+MariamYHanna@users.noreply.github.com>
2023-08-16 07:30:03 +1000

    start 8 week system

---
+++ week_system.php
@@ -0,0 +1,27 @@
+<?php require_once('header.php');?>
+<title>8 Week System | Consult CRM</title>
+            <!-- MAIN CONTENT-->
+            <div class="main-content">
+                <div class="section__content section__content--p30">
+                    <h1 style="text-align: center;"><u>OPTION 1</u></h1>
+                    <div class="container-fluid">
+                        <h2>Step-1</h2> 
+                        <br>
+                        <div class="row">
+                            
+                            <div class="col-md-4">
+                                <div class="card">
+                                    <div class="card-header bg-dark">
+                                        <strong class="card-title text-light">Client Data Form</strong>
+                                    </div>
+                                    <div class="card-body text-white bg-primary">
+                                        <p class="card-text text-light">Checkout all the details of the data of client required
+                                        </p>
+                                        <br>
+                                        <a class="btn btn-dark" href="#">Link To Another Page</a>
+                                    </div>
+                                </div>
+                            </div>
+                            </body>
+
+</html>                            
