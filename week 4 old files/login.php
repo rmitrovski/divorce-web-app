18ea4596447a3b96c2076b498f489facbf2cfd5c
Robert <s3901069@student.rmit.edu.au>
2023-08-16 18:30:31 +1000

    Added login sessions and logout feature

--- week 4 old files/login.php
+++ week 4 old files/login.php
@@ -1,4 +1,5 @@
 <?php
+session_start(); // Start a session
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $password = $_POST['password'];
@@ -8,15 +9,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($conn->connect_error) {
         die("Connection Failed: " . $conn->connect_error);
     } else {
-        $stmt = $conn->prepare("SELECT password FROM persons WHERE email = ?");
+        $stmt = $conn->prepare("SELECT password, fname, bio FROM persons WHERE email = ?");
         $stmt->bind_param("s", $email);
         $stmt->execute();
-        $stmt->bind_result($hashedPassword);
+        $stmt->bind_result($hashedPassword, $fname, $bio);
         $stmt->fetch();
 
         if (password_verify($password, $hashedPassword)) {
             echo "Login successful...";
-             header("Location: index.html");
+            $_SESSION['email'] = $email;
+            $_SESSION['fname'] = $fname;
+            $_SESSION['bio'] = $bio;
+            header("Location: index.html");
+            exit();
         } else {
             echo "Invalid login credentials.";
         }
