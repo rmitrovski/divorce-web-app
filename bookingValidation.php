b968a8e9d448571f6be2482a9a8181f145a38c8b
Utepilsss <114961124+Utepilsss@users.noreply.github.com>
2023-08-21 13:50:26 +1000

    Update bookingValidation.php

--- bookingValidation.php
+++ bookingValidation.php
@@ -1,6 +1,4 @@
 <?php
-session_start(); // Start the session
-
 // Initialize the bookedSlots array if it doesn't exist in the session
 if (!isset($_SESSION['bookedSlots'])) {
     $_SESSION['bookedSlots'] = [];
@@ -9,7 +7,25 @@ if (!isset($_SESSION['bookedSlots'])) {
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $selectedDate = $_POST["date"];
     $selectedTime = $_POST["time"];
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+
+  if (!preg_match("/^[a-zA-Z\s]*$/", $name)) {
+    echo "<script>alert('Name can only contain letters and spaces.');</script>";
     
+}
+
+  if (!preg_match("/[^0-9\s\-\+\(\)]/", $phone)) {
+        
+    }
+
+   if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "<script>alert('Invalid email format.');</script>";
+       
+    }
+
+      
     // Check if the selected date and time are already booked
     $isAlreadyBooked = false;
     foreach ($_SESSION['bookedSlots'] as $slot) {
@@ -26,9 +42,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo json_encode(['message' => 'Booking successful']);
     } else {
         // Respond to the client with an error message
-        echo json_encode(['message' => 'Slot already booked']);
+       echo "<script>
+        document.getElementById('alertTitle').innerText = 'Booking Time Alert';
+        document.getElementById('alertMessage').innerText = 'Booking already exists for this date and time. Please choose another time.';
+        document.getElementById('customAlert').style.display='block';
+      </script>";
+
+
     }
 }
+
+
+
 
 // Display the content of the bookedSlots array in the session
 echo "Booked Slots:<br>";
