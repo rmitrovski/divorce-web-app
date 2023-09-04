af249b6c61f76739cbd591b57ff2c8510146ecf0
Utepilsss <114961124+Utepilsss@users.noreply.github.com>
2023-09-04 10:18:30 +1000

    revise some issues

--- bookingValidation.php
+++ bookingValidation.php
@@ -1,5 +1,6 @@
 
 <?php
+session_start();
 
 $response = array(
     'success' => false,
@@ -8,30 +9,49 @@ $response = array(
     'bookedSlots' => []
 );
 
+
 if (!isset($_SESSION['bookedSlots'])) {
     $_SESSION['bookedSlots'] = [];
 }
 
+
+
+
+if (!isset($_SESSION['name'])) {
+    $_SESSION['name'] = [];
+}
+
+if (!isset($_SESSION['phone'])) {
+    $_SESSION['phone'] = [];
+}
+
+if (!isset($_SESSION['email'])) {
+    $_SESSION['email'] = [];
+}
+
+if (!isset($_SESSION['reason'])) {
+    $_SESSION['reason'] = [];
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
     $selectedDate = $_POST["date"];
     $selectedTime = $_POST["time"];
     $name = $_POST['name'];
     $phone = $_POST['phone'];
+    $reason = $_POST['reason'];
     $email = $_POST['email'];
 
     if (!preg_match("/^[a-zA-Z\s]*$/", $name)) {
         $response['errors'][] = 'Name can only contain letters and spaces.';
-       echo "<script>alert('Name can only contain letters and spaces.');</script>";
     }
 
     if (!preg_match("/^[0-9]+$/", $phone)) {
         $response['errors'][] = 'Invalid phone format.';
-        echo "<script>alert('Invalid phone format.');</script>";
     }
 
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $response['errors'][] = 'Invalid email format.';
-       echo "<script>alert('Invalid email format.');</script>";
     }
 
     // Check if the selected date and time are already booked
@@ -44,6 +64,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     if (empty($response['errors'])) {
+        $_SESSION['name'][] = $name;
+        $_SESSION['phone'][] = $phone;
+        $_SESSION['email'][] = $email;
+        $_SESSION['reason'][] = $reason;
         if (!$isAlreadyBooked) {
             // Store the booked slot in the session
             $_SESSION['bookedSlots'][] = ['date' => $selectedDate, 'time' => $selectedTime];
@@ -51,24 +75,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $response['message'] = 'Booking successful';
         } else {
             $response['errors'][] = 'Booking already exists for this date and time. Please choose another time.';
-           echo "<script>
-        document.getElementById('alertTitle').innerText = 'Booking Time Alert';
-        document.getElementById('alertMessage').innerText = 'Booking already exists for this date and time. Please choose another time.';
-        document.getElementById('customAlert').style.display='block';
-      </script>";
         }
     }
 
-    $response['bookedSlots'] = $_SESSION['bookedSlots'];  // Return all booked slots to the client
+   
+
+    $response['bookedSlots'] = $_SESSION['bookedSlots'];  
 }
 
 
-echo json_encode($response);
-
-
-// Display the content of the bookedSlots array in the session
-echo "Booked Slots:<br>";
-foreach ($_SESSION['bookedSlots'] as $slot) {
-    echo "Date: " . $slot['date'] . ", Time: " . $slot['time'] . "<br>";
-}
 ?>
+
