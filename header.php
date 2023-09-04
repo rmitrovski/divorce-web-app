b3f435ac191719af33444722c6dccdeea36e5a25
Mariam Hanna <115123087+MariamYHanna@users.noreply.github.com>
2023-09-04 19:45:59 +1000

    adjust layout

--- header.php
+++ header.php
@@ -59,6 +59,48 @@
            
         </header>
         <!-- END HEADER MOBILE-->
+          <!-- PAGE CONTAINER-->
+          <div class="page-container">
+            <!-- HEADER DESKTOP-->
+            <header class="header-desktop">
+                <div class="section__content section__content--p30">
+                    <div class="container-fluid">
+                        <div class="header-wrap">
+                            <form class="form-header" action="" method="POST">
+                                <input class="au-input au-input--xl" type="text" name="search" placeholder="Search..." />
+                                <button class="au-btn--submit" type="submit">
+                                <i class="zmdi zmdi-search"></i>
+
+                                </button>
+                            </form>
+                            <div class="header-button">
+                                <div class="noti-wrap">
+                                </div>
+                                <div class="account-wrap">
+                                    <div class="account-item clearfix js-item-menu">
+                                        <div class="image">
+                                            <img src="https://cdn-icons-png.flaticon.com/512/3177/3177440.png" alt="John Doe" />
+                                        </div>
+                                       
+                                        <div class="account-dropdown js-dropdown">
+                                            <div class="info clearfix">
+                                                
+                                                
+                                            </div>
+                                            
+                                               
+                                            
+    
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <!-- HEADER DESKTOP-->
 
         <!-- MENU SIDEBAR-->
         
@@ -119,45 +161,4 @@
 
         <!-- END MENU SIDEBAR-->
 
-        <!-- PAGE CONTAINER-->
-        <div class="page-container">
-            <!-- HEADER DESKTOP-->
-            <header class="header-desktop">
-                <div class="section__content section__content--p30">
-                    <div class="container-fluid">
-                        <div class="header-wrap">
-                            <form class="form-header" action="" method="POST">
-                                <input class="au-input au-input--xl" type="text" name="search" placeholder="Search..." />
-                                <button class="au-btn--submit" type="submit">
-                                <i class="zmdi zmdi-search"></i>
-
-                                </button>
-                            </form>
-                            <div class="header-button">
-                                <div class="noti-wrap">
-                                </div>
-                                <div class="account-wrap">
-                                    <div class="account-item clearfix js-item-menu">
-                                        <div class="image">
-                                            <img src="https://cdn-icons-png.flaticon.com/512/3177/3177440.png" alt="John Doe" />
-                                        </div>
-                                       
-                                        <div class="account-dropdown js-dropdown">
-                                            <div class="info clearfix">
-                                                
-                                                
-                                            </div>
-                                            
-                                               
-                                            
-    
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </header>
-            <!-- HEADER DESKTOP-->
+      
