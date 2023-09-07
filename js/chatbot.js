b3470de0180ab987d3ce52e23d84a809e427fd35
Mariam Hanna <115123087+MariamYHanna@users.noreply.github.com>
2023-09-07 20:02:23 +1000

    basic chat functions for chatbot

---
+++ js/chatbot.js
@@ -0,0 +1,132 @@
+const chatInput= document.querySelector("#chat-input");
+const sendButton= document.querySelector("#send-btn");
+const chatContainer= document.querySelector(".chat-container");
+const themeButton= document.querySelector("#theme-btn");
+const deleteButton= document.querySelector("#delete-btn");
+let userText=null;
+const API_KEY="";
+
+const loadDataFromLocalstorage=()=>{
+    const themeColor= localStorage.getItem("themeColor");
+    document.body.classList.toggle("light_mode", themeColor === "light_mode");
+    themeButton.innerText= document.body.classList.contains("light_mode") ? "dark_mode":"light_mode";
+
+    const defaultText= ''
+    chatContainer.innerHTML=localStorage.getItem("all-chats") || defaultText;
+    chatContainer.scrollTo(0,chatContainer.scrollHeight);
+
+
+}
+
+const createChatElement=(content, className)=>{
+    const chatDiv=document.createElement("div");
+    chatDiv.classList.add("chat", className);
+    chatDiv.innerHTML=connect;
+    return chatDiv;
+}
+
+const getChatResponse = async (incomingChatDiv) => {
+    const API_URL="";
+    const pElement= document.createElemnet("p");
+
+    const requestOptions={
+        method: "POST", 
+        headers:{
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${API_KEY}`
+        }, 
+        BODY: JSON.stringify({
+            model:"text-davinci-003",
+            prompt: userText, 
+            max_tokens:2048, 
+            temperature:0.2,
+            n: 1,
+            stop: null
+        })
+    }
+
+
+    try{ 
+        const response=await(await fetch (API_URL, requestOptions)).json();
+        pElement.textContent= response.choices[0].text.trim();
+
+    }catch(error){
+        pElement.classList.add("error");
+        pElement.textContent="Please try again";
+
+
+    }
+
+    incomingChatDiv.querySelector(".typing-animation").remove();
+    incomingChatDiv.querySelector(".chat-details").appendChild(pElement);
+    localStorage.setItem("all-chats", chatContainer.innerHTML);
+    chatContainer.scrollTo(0,chatContainer.scrollHeight);
+
+}
+
+const copyResponse=(copyBtn)=>{
+    const responseTextElement= copyBtn.parentElement.querySelector("p");
+    navigator.clipboard.writeText(responseTextElement.textContent);
+    copyBtn.textContent="done";
+    setTimeout(() => copyBtn.textContent = "content_copy", 1000);
+}
+
+const showTypingAnimation=()=>{
+    const html='';
+    const incomingChatDiv= createChatElement(html,"incoming");
+    chatContainer.appendChild(incomingChatDiv);
+    chatContainer.scrollTo(0,chatContainer.scrollHeight);
+    getChatResponse(incomingChatDiv);
+
+}
+
+
+const handleOutgoingChat=()=>{
+    userText = chatInput.value.trim();
+    if(!userText) return;
+
+    chatInput.value="";
+    chatInput.style.height= `${initialInputHeight}px`;
+
+    const html='';
+    const outgoingChatDiv=createChatElement(html,"outgoing");
+    chatContainer.querySelector(".default-text")?.remove();
+    chatContainer.appendChild(outgoingChatDiv);
+    chatContainer.scrollTo(0,chatContainer.scrollHeight);
+    setTimeout(showTypingAnimation, 500);
+
+}
+
+deleteButton.addEventListener("click", () =>{
+    if(confirm("Are u sure u want to delet all the chatsL")){
+        localStorage.removeItem("all-chats");
+        loadDataFromLocalstorage();
+    }
+});
+
+themeButton.addEventListener("click", ()=>{
+    document.body.classList.toggle("light-mode");
+    localStorage.setItem("themeColor", themeButton.innerText);
+    themeButton.innerText= document.body.classList.contains("light-mode")? "dark_mode": "light_mode";
+
+});
+
+const initialInputHeight=  chatInput.scrollHeight;
+
+chatInput.addEventListener("input",()=>{
+    chatInput.style.height= `${initialInputHeight}px`;
+    chatInput.style.height= `${chatInput.scrollHeight}px`;
+
+});
+
+chatInput.addEventListener("Keydown", (e)=>{
+    if (e.key == "Enter" && !e.shiftKey && window.innerWidth >800){
+        e.preventDefault();
+        handleOutgoingChat();
+    }
+
+});
+
+loadDataFromLocalstorage();
+sendButton.addEventListener("click", handleOutgoingChat);
+
