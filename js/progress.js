47335d042a33f50bbe27d3f68fb3e86bf06f1e9a
Mariam Hanna <115123087+MariamYHanna@users.noreply.github.com>
2023-09-09 11:01:55 +1000

    created JS functions for progress bar

---
+++ js/progress.js
@@ -0,0 +1,43 @@
+const circles=document.querySelectorAll(".circle"), 
+ progressBar=document.querySelector(".indicator"), 
+ buttons=document.querySelectorAll("button");
+let currentStep=1; 
+
+const updateSteps=(e)=>{
+    currentStep = e.target.id === "next" ? ++currentStep : -- currentStep;
+    circles.forEach((circle,index)=>{
+        circle.classList[`${index < currentStep ? "add" : "remove"}`]("active");
+
+    });
+    progressBar.style.width = `${((currentStep-1)/ (circles.length -1))* 100}%`;
+
+
+    if (currentStep === circles.length){
+        buttons[1].disabled = true;
+
+    }else if(currentStep === 1){
+        buttons[0].disabled = true;
+
+    }else{
+        buttons.forEach((button)=> ( button.disabled = false));
+
+    }
+
+
+
+
+
+};
+
+
+buttons.forEach((button)=>{
+    button.addEventListener("click", updateSteps);
+
+});
+
+
+
+
+
+
+
