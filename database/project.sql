be7f1d5c0dfd8a3998f1e372668fff33dab53f31
NoParslyPlz <105872143+NoParslyPlz@users.noreply.github.com>
2023-09-13 17:16:39 +1000

    Update project.sql

--- database/project.sql
+++ database/project.sql
@@ -5,3 +5,16 @@ CREATE TABLE `users` (
     `password` varchar(100) NOT NULL,
     `registration_date` DATE NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `bookings` (
+    `booking_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name` varchar(100) NOT NULL,
+    `phone` int NOT NULL,
+    `email` varchar(100) NOT NULL,
+    `type` varchar(100) NOT NULL,
+    `date` DATE NOT NULL,
+    `time` varchar(100) NOT NULL,
+    `reason` varchar(200) NOT NULL,
+    `user_id` int(11) NOT NULL,
+    FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1; 
