5ad7a2a5b21fc9907b3ed32cf2a379929e84fe10
Mariam Hanna <115123087+MariamYHanna@users.noreply.github.com>
2023-09-13 18:27:30 +1000

    Update week_system.php

--- week_system.php
+++ week_system.php
@@ -13,7 +13,7 @@ if (!isset($_SESSION['username'])) {
 <html lang="en">
 
 <head>
-    <!-- Required meta tags 9.30-11 12.30-1.40-->
+    <!-- Required meta tags -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
