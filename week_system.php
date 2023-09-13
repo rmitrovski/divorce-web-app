250c45cc5c214beedddf2d2db108d51f1f4a4606
Mariam Hanna <115123087+MariamYHanna@users.noreply.github.com>
2023-09-13 18:19:08 +1000

    Update week_system.php

--- week_system.php
+++ week_system.php
@@ -24,7 +24,7 @@ if (!isset($_SESSION['username'])) {
     
 	<link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css"/>
  
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/dimention.css">
 
 
 
@@ -150,7 +150,7 @@ if (!isset($_SESSION['username'])) {
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
     <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
-    <script src="script.js"></script>
+    <script src="js/dimension.js"></script>
 
 
 </body>
