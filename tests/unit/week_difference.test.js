856a7d7e6a73e5c338bb6208344f6689df4d786c
Robert <s3901069@student.rmit.edu.au>
2023-09-16 21:37:00 +1000

    added a new test

---
+++ tests/unit/week_difference.test.js
@@ -0,0 +1,11 @@
+const calculateWeekDifference = require('../WeekDifferentCalculator.js');
+
+test('calculates week difference correctly', () => {
+  const registrationDate = new Date();
+  registrationDate.setDate(registrationDate.getDate() - 28);
+
+  const weeksDifference = calculateWeekDifference(registrationDate);
+
+  expect(weeksDifference).toBe(4);
+});
+
