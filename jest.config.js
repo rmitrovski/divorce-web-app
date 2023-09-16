933f2837de80958438905e679cf7ec4725b61a73
Mariam Hanna <115123087+MariamYHanna@users.noreply.github.com>
2023-09-16 15:41:54 +1000

    package.json, eslintrc and jest.config for jest tesing

---
+++ jest.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+
+    clearMocks: true,
+    collectCoverage: true,
+    collectCoverageFrom: ["models/*.js"],
+    coverageDirectory: "coverage/",
+    coveragePathIgnorePatterns: ["/node_modules/"],
+    coverageReporters: ["json", "text", "lcov"],
+    testEnvironment: 'node',
+    reporters: [
+    'default',
+    ['jest-junit', { outputDirectory: 'reports',
+    outputName: 'report.xml' }],
+    ],
+    testMatch: [
+        "**/tests/unit/**/*.test.js",
+        "**/__tests__/**/*.spec.js"
+        ],
+        collectCoverage: true,
+        clearMocks: true,
+        collectCoverageFrom: ["models/*.js"],
+        coveragePathIgnorePatterns: [
+            "/node_modules/"
+            ],
+      
+
+  
+    };
+
+
+    
