88cf79c01570510416d62b8ab32ddc4c09faffe1
Robert <s3901069@student.rmit.edu.au>
2023-09-17 22:16:27 +1000

    added another test

--- tests/unit/week_difference.test.js
+++ tests/unit/week_difference.test.js
@@ -9,3 +9,12 @@ test('calculates week difference correctly', () => {
   expect(weeksDifference).toBe(4);
 });
 
+test('calculates week difference for 8 weeks', () => {
+  const registrationDate = new Date();
+  registrationDate.setDate(registrationDate.getDate() - 56); // Subtract 56 days for 8 weeks
+
+  const weeksDifference = calculateWeekDifference(registrationDate);
+
+  expect(weeksDifference).toBe(8);
+});
+
