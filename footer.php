3d4f66d83f1414fcdd0d59724edc3fb0bb7b61fa
Utepilsss <114961124+Utepilsss@users.noreply.github.com>
2023-09-17 14:28:10 +1000

    adjust style of footer

--- footer.php
+++ footer.php
@@ -1,39 +1,40 @@
- 
-    <div class= "row">
-        <div class="col-md-12">
-        <div class="copyright">
-            <p>Copyright @ 2023 LAW CRM. All rights reserves. </p>
-            </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+<div class="container-fluid" style="background-color: lightblue; color: white; padding-bottom: 40px; min-height: 150px;">
+    <div class="row align-items-center">
+        <div class="col-md-6 text-center py-3">
+            <p class="mb-0" style="font-size: 1.5em; font-weight: bold;">Suzanne Winlove-Smith</p>
+            <p class="mb-0" style="font-size: 1.2em;">Divorce Coach & Advisor</p>
+            <p class="mb-0" style="font-size: 1.2em;">613-805-0022</p>
+            <a href="https://instagram.com" target="_blank" class="text-white">
+                <i class="fab fa-instagram fa-lg" style="margin-top: 10px;"></i>
+            </a>
         </div>
- </div>
- <!-- Jquery JS-->
-    <script src="vendor/jquery-3.2.1.min.js"></script>
-    <!-- Bootstrap JS-->
-    <script src="vendor/bootstrap-4.1/popper.min.js"></script>
-    <script src="vendor/bootstrap-4.1/bootstrap.min.js"></script>
-    <!-- Vendor JS       -->
-    <script src="vendor/slick/slick.min.js">
-    </script>
-    <script src="vendor/wow/wow.min.js"></script>
-    <script src="vendor/animsition/animsition.min.js"></script>
-    <script src="vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
-    </script>
-    <script src="vendor/counter-up/jquery.waypoints.min.js"></script>
-    <script src="vendor/counter-up/jquery.counterup.min.js">
-    </script>
-    <script src="vendor/circle-progress/circle-progress.min.js"></script>
-    <script src="vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
-    <script src="vendor/chartjs/Chart.bundle.min.js"></script>
-    <script src="vendor/select2/select2.min.js">
-    </script>
+        <div class="col-md-6 text-center py-3">
+            <form action="subscribe.php" method="POST" class="d-flex justify-content-center" style="align-items: center;">
+                <input type="email" name="email" placeholder="Enter your email" class="form-control mb-2" style="width: 70%; margin-right: 10px; border-radius: 5px;">
+                <button type="submit" class="btn btn-primary mb-2" style="border-radius: 5px;">Subscribe</button>
+            </form>
+        </div>
+    </div>
+</div>
 
-    <!-- Main JS-->
-    <script src="js/script.js"></script>
-    
-    </body>
-    </html>
+<!-- Jquery JS-->
+<script src="booking/vendor/jquery-3.2.1.min.js"></script>
+<!-- Bootstrap JS-->
+<script src="booking/vendor/bootstrap-4.1/popper.min.js"></script>
+<script src="booking/vendor/bootstrap-4.1/bootstrap.min.js"></script>
+<!-- Vendor JS-->
+<script src="booking/vendor/slick/slick.min.js"></script>
+<script src="booking/vendor/wow/wow.min.js"></script>
+<script src="booking/vendor/animsition/animsition.min.js"></script>
+<script src="booking/vendor/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+<script src="booking/vendor/counter-up/jquery.waypoints.min.js"></script>
+<script src="booking/vendor/counter-up/jquery.counterup.min.js"></script>
+<script src="booking/vendor/circle-progress/circle-progress.min.js"></script>
+<script src="booking/vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
+<script src="booking/vendor/chartjs/Chart.bundle.min.js"></script>
+<script src="booking/vendor/select2/select2.min.js"></script>
+
+<!-- Main JS-->
+<script src="js/script.js"></script>
+
+</body>
